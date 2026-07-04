85d2edc149218dbfa92d46233a781c33c7be5709
dsward2 <dsward@gmail.com>
2026-07-04 10:24:24 -0500

    Initial Commit

---
+++ ControlBooth/ControlBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlBoothApp.swift
+//  ControlBooth
+//
+//  Created by Douglas Ward on 7/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ControlBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ControlBoothTests/ControlBoothTests.swift
@@ -0,0 +1,19 @@
+//
+//  ControlBoothTests.swift
+//  ControlBoothTests
+//
+//  Created by Douglas Ward on 7/4/26.
+//
+
+import Testing
+@testable import ControlBooth
+
+struct ControlBoothTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
